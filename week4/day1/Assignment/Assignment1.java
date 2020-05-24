42e36d0853af251a3a54258ee29a150e7e68b22d
vijiganesan <64795175+vijiganesan@users.noreply.github.com>
2020-05-24 12:52:04 +0100

    Add files via upload
    updated the comments

--- week4/day1/Assignment/Assignment1.java
+++ week4/day1/Assignment/Assignment1.java
@@ -21,27 +21,32 @@ public class Assignment1 {
 		ChromeDriver driver = new ChromeDriver(options);
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+//1) Launch URL: https://acme-test.uipath.com/account/login
 		driver.get("https://acme-test.uipath.com/account/login");
 		
 		driver.manage().window().maximize();
-		
+//2) Enter UserName (kumar.testleaf@gmail.com) and TAB		
 		driver.findElementById("email").sendKeys("kumar.testleaf@gmail.com");
+//3) Enter Password (leaf@12)
 		driver.findElementById("password").sendKeys("leaf@12");
+//4) Click Login
 		driver.findElementById("buttonLogin").click();
-		
+//5) Mouse Over on Vendors		
 		Actions builder = new Actions(driver);
 		
 		builder.moveToElement(driver.findElementByXPath("(//div[@class='dropdown']/button[@class='btn btn-default btn-lg'])[3]")).perform();
 		Thread.sleep(500);
 		//System.out.println("1");
+//6) Click Search Vendor		
 		driver.findElementByXPath("(//ul[@class='dropdown-menu dropdown-content rightMenu'])[4]//a").click();
 		Thread.sleep(500);
 		//System.out.println("2");
+//7) Enter Vendor Name (Blue Lagoon)
 		driver.findElementById("vendorName").sendKeys("Blue Lagoon");
-		
+//8) Click Search		
 		driver.findElementByXPath("//button[@class='btn btn-primary']").click();
 		
+//9) Find the Country Name based on the Vendor		
 		
 		List<WebElement> rows = driver.findElementsByXPath("//table//tr");
 		int rowCount = rows.size();
@@ -67,7 +72,11 @@ public class Assignment1 {
 		}
 		
 		Thread.sleep(1000);
+//10) Click Log Out		
 		
+		driver.findElementByXPath("//a[text()='Log Out']").click();
+		Thread.sleep(1000);
+//11) Close browser		
 		driver.close();
 	}
 	
